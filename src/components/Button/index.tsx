afc050dfd4a7ddeff787b18d117b3e80f48b1264
Phillippe Rocha <phillipperocha@gmail.com>
2021-04-16 13:02:45 -0300

    Adding styled-component, creating global style and change baseUrl

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,5 @@
-const Button = () => <button type="button">Snack Shop</button>
+import React from 'react'
+
+const Button: React.FC = () => <button type="button">Snack Shop</button>
 
 export default Button
